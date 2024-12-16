06938fc4a7f8517e15ff487b3ad1768967bf800c
kcavs <159186856+kcavs@users.noreply.github.com>
2024-12-16 14:38:54 -0500

    hi

--- SgRandomWalk.java
+++ SgRandomWalk.java
@@ -1,18 +1,30 @@
 public class SgRandomWalk{
     public static void main(String[]args){
+        boolean escaped=false;
+        int tries=0;
         Rectangle field=new Rectangle(0,0,300,300);
         field.setColor(Color.BLUE);
         field.draw();
         field.fill();
-        Ellipse circle=new Ellipse(150,150,100,100);
+        Ellipse circle=new Ellipse(100,100,100,100);
         circle.setColor(Color.PINK);
         circle.draw();
-        circle.fill();
-        Ellipse dot= new Ellipse(150,150,10,10);
+        Ellipse dot= new Ellipse(125,125,10,10);
         dot.setColor(Color.MAGENTA);
         dot.draw();
         dot.fill();
-        int randy=(int)((Math.random()*21)-10);
-        int randx=(int)((Math.random()*21)-10);
+        while(escaped==false){
+            try { Thread.sleep(100);}
+            catch(Exception ex) {};
+            int randy=(int)((Math.random()*21)-10);
+            int randx=(int)((Math.random()*21)-10);
+            tries+=1;
+            dot.translate(randx,randy);
+            double dist=Math.sqrt(((randx-150)*(randx-150))+((randy-150)*(randy-150)));
+            if(dist>100){
+                escaped=true;
+                System.out.println("it took "+tries+" to escape");
+            }
+        }
     }
 }
